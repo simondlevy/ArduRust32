510b76025ec994c9acac3f448c4aba52ce5f0b3b
simondlevy <simon.d.levy@gmail.com>
2023-02-22 23:37:26 -0500

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#[no_mangle]
+pub extern fn add(first: i32, second: i32) -> i32
+{
+        first + second
+}
+
---
+++ src/test_lib.h
@@ -0,0 +1,11 @@
+#pragma once
+ 
+#ifdef __cplusplus
+extern "C"{
+#endif
+     
+    int add(int first, int second);
+     
+#ifdef __cplusplus
+}
+#endif
